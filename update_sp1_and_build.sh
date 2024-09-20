5b4ae5b963babad13229c605bb5c9d2cabab649d
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2024-09-20 14:22:05 -0700

    clean up inconsistencies with macos sed

--- update_sp1_and_build.sh
+++ update_sp1_and_build.sh
@@ -18,26 +18,27 @@ cp "$CARGO_TOML" "$CARGO_TOML.bak"
 # List of SP1 dependencies to update
 SP1_DEPS=("sp1-prover" "sp1-core-executor" "sp1-core-machine" "sp1-cuda" "sp1-stark")
 
-# Determine the sed in-place flag based on the OS
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED_IN_PLACE=(-i "")
-else
-    SED_IN_PLACE=(-i)
-fi
-
 # Update the Cargo.toml file
 if [[ $SP1_REF =~ ^[0-9a-f]{7,40}$ ]]; then
     # It's a commit hash
     for dep in "${SP1_DEPS[@]}"; do
-        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*branch = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", rev = \"$SP1_REF\" }|" "$CARGO_TOML"
-        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*rev = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", rev = \"$SP1_REF\" }|" "$CARGO_TOML"
+        awk -v dep="$dep" -v ref="$SP1_REF" '
+        $0 ~ "^"dep" = { git = \"https://github.com/succinctlabs/sp1" {
+            gsub(/(branch|rev) = "[^"]*"/, "rev = \""ref"\"")
+        }
+        { print }
+        ' "$CARGO_TOML" > "$CARGO_TOML.tmp" && mv "$CARGO_TOML.tmp" "$CARGO_TOML"
     done
     echo "Updated Cargo.toml with new SP1 commit hash"
 else
     # It's a branch name
     for dep in "${SP1_DEPS[@]}"; do
-        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*branch = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", branch = \"$SP1_REF\" }|" "$CARGO_TOML"
-        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*rev = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", branch = \"$SP1_REF\" }|" "$CARGO_TOML"
+        awk -v dep="$dep" -v ref="$SP1_REF" '
+        $0 ~ "^"dep" = { git = \"https://github.com/succinctlabs/sp1" {
+            gsub(/(branch|rev) = "[^"]*"/, "branch = \""ref"\"")
+        }
+        { print }
+        ' "$CARGO_TOML" > "$CARGO_TOML.tmp" && mv "$CARGO_TOML.tmp" "$CARGO_TOML"
     done
     echo "Updated Cargo.toml with new SP1 branch"
 fi
