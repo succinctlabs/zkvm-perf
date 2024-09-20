8118aa177d9e85a6e1cbd5616b1723da7504ca3f
Conner Swann <2635475+yourbuddyconner@users.noreply.github.com>
2024-09-20 13:44:05 -0700

    bugfix for cargo rewriting

--- update_sp1_and_build.sh
+++ update_sp1_and_build.sh
@@ -12,26 +12,39 @@ RUN_BUILD=${RUN_BUILD:-"false"}
 # Path to the Cargo.toml file
 CARGO_TOML="eval/Cargo.toml"
 
+# Create a backup of the original file
+cp "$CARGO_TOML" "$CARGO_TOML.bak"
+
 # List of SP1 dependencies to update
 SP1_DEPS=("sp1-prover" "sp1-core-executor" "sp1-core-machine" "sp1-cuda" "sp1-stark")
+
+# Determine the sed in-place flag based on the OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED_IN_PLACE=(-i "")
+else
+    SED_IN_PLACE=(-i)
+fi
 
 # Update the Cargo.toml file
 if [[ $SP1_REF =~ ^[0-9a-f]{7,40}$ ]]; then
     # It's a commit hash
     for dep in "${SP1_DEPS[@]}"; do
-        sed -i.bak "s#^$dep = { git = \"https://github.com/succinctlabs/sp1.*\(branch = \"[^\"]+\"|rev = \"[^\"]+\"\)#$dep = { git = \"https://github.com/succinctlabs/sp1\", rev = \"$SP1_REF\"#" "$CARGO_TOML"
+        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*branch = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", rev = \"$SP1_REF\" }|" "$CARGO_TOML"
+        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*rev = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", rev = \"$SP1_REF\" }|" "$CARGO_TOML"
     done
     echo "Updated Cargo.toml with new SP1 commit hash"
 else
     # It's a branch name
     for dep in "${SP1_DEPS[@]}"; do
-        sed -i.bak "s#^$dep = { git = \"https://github.com/succinctlabs/sp1.*\(branch = \"[^\"]+\"|rev = \"[^\"]+\"\)#$dep = { git = \"https://github.com/succinctlabs/sp1\", branch = \"$SP1_REF\"#" "$CARGO_TOML"
+        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*branch = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", branch = \"$SP1_REF\" }|" "$CARGO_TOML"
+        sed "${SED_IN_PLACE[@]}" "s|^$dep = { git = \"https://github.com/succinctlabs/sp1.*rev = \"[^\"]*\".*$|$dep = { git = \"https://github.com/succinctlabs/sp1\", branch = \"$SP1_REF\" }|" "$CARGO_TOML"
     done
     echo "Updated Cargo.toml with new SP1 branch"
 fi
 
-# Show the diff
-diff "$CARGO_TOML.bak" "$CARGO_TOML" || true
+# Show the full diff
+echo "Full diff of Cargo.toml changes:"
+diff -u "$CARGO_TOML.bak" "$CARGO_TOML" || true
 
 # Remove the backup file
 rm "$CARGO_TOML.bak"
